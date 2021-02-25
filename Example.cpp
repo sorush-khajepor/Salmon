a65a01ee98d1d81f1562bb14289cdff0bfc76c99
Sorush Khajepor <sorush.kh@gmail.com>
2021-02-25 00:40:11 +0000

    update

--- Example.cpp
+++ Example.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <functional>
 #include <filesystem>
 #include "include/Salmon.h"
 
@@ -18,18 +16,28 @@ int main(){
 
   editor.file.Open("sample.txt");
   
+  // Check if file starts with a specefic signature
+  if (editor.content.StartsWith("Edinburgh")){
+    // do stuff
+  };
+
+
   // Editing lines
   editor.content.AppendLine("I like haggis.");
-  editor.content.InsertLine("I am looking forward to summer.",3);
-  editor.content.ReplaceLine(2,"A usual rainy day in Edinburgh.");
+  editor.content.InsertLine("I am looking forward to summer.", 3);
+  editor.content.ReplaceLine(2, "A usual rainy day in Edinburgh.");
   editor.content.DeleteLine(1);
 
   // Editing text
   editor.content.AppendText(3, " It's OK.");
-  editor.content.DeleteText(4, 9, 21-9+1);
+  editor.content.DeleteText(4, 9, 13);
   editor.content.ReplaceText(1,3,11,"rare warm sunny");
   editor.content.InsertText(3, 43, ", because of covid-19");
   editor.content.FindReplace("haggis", "Irn Bru");
+
+  // Copy-Paste
+  editor.content.Copy(1,26,2,24);
+  editor.content.Paste(5,8);
 
   // save the file
   editor.file.Save();
--- src/Content.h
+++ src/Content.h
@@ -4,42 +4,48 @@
 #include <vector>
 #include <functional>
 #include <filesystem>
-#include <ranges>
+//#include <ranges>
 
 namespace Salmon
 {
     class Content
     {
 
-        std::vector<std::string> data;
+        std::vector<std::string> lines;
+        std::vector<std::string> clipboard;
 
     public:
         auto Clear()
         {
-            data.clear();
+            lines.clear();
         }
 
-        auto begin() { return data.begin(); }
+        auto begin() { return lines.begin(); }
 
-        auto end() { return data.end(); }
+        auto end() { return lines.end(); }
 
         auto getDataIter(int iLine)
         {
-            return data.begin() + iLine - 1;
+            return lines.begin() + iLine - 1;
+        }
+
+        auto StartsWith(std::string line)
+        {
+            return lines[0].rfind(line, 0) == 0;
         }
 
         auto AppendLine(std::string line)
         {
-            data.push_back(line);
+            lines.push_back(line);
         }
         auto ReplaceLine(int iLine, std::string line)
         {
-            data[iLine - 1] = line;
+            lines[iLine - 1] = line;
         }
 
         auto DeleteLine(int iLine)
         {
-            data.erase(getDataIter(iLine));
+            lines.erase(getDataIter(iLine));
         }
 
         auto DeleteLine(std::vector<size_t> iLines)
@@ -57,34 +63,61 @@ namespace Salmon
 
         auto InsertLine(std::string line, int iLine)
         {
-            data.insert(getDataIter(iLine), line);
+            lines.insert(getDataIter(iLine), line);
         }
 
         auto AppendText(size_t iLine, std::string word)
         {
-            data[iLine - 1].append(word);
+            lines[iLine - 1].append(word);
         }
 
         auto DeleteText(size_t iLine, size_t iCol, size_t lenght)
         {
-            data[iLine - 1].erase(iCol - 1, lenght);
+            lines[iLine - 1].erase(iCol - 1, lenght);
         }
 
         auto InsertText(size_t iLine, size_t iCol, std::string word)
         {
-            data[iLine - 1].insert(iCol - 1, word);
+            lines[iLine - 1].insert(iCol - 1, word);
         }
 
         auto ReplaceText(size_t iLine, size_t iCol, size_t length, std::string word)
         {
-            data[iLine - 1].replace(iCol - 1, length, word);
+            lines[iLine - 1].replace(iCol - 1, length, word);
+        }
+
+        auto Copy(size_t iFromLine, size_t iFromCol, size_t iToLine, size_t iToCol )
+        {
+            clipboard.clear();
+            clipboard.push_back(lines[iFromLine-1].substr(iFromCol-1));
+            for(size_t iLine=iFromLine+1;iLine<iToLine;iLine++){
+                clipboard.push_back(lines[iLine]);
+            }
+            clipboard.push_back(lines[iToLine-1].substr(0, iToCol));
+                
+        }
+        auto Paste(size_t iLine, size_t iCol)
+        {
+            auto rest = lines[iLine-1].substr(iCol-1);
+            lines[iLine-1].erase(iCol-1);
+            InsertText(iLine, iCol, clipboard[0]);
+            for(size_t i=1;i<clipboard.size()-1;i++){
+                InsertLine(clipboard[i], iLine+i);
+            }
+            auto last = iLine+clipboard.size()-1;
+            if (lines.size()>= last){
+                InsertText(last, 1, clipboard.back());
+            } else{
+                InsertLine(clipboard.back(),last);
+            }
+            AppendText(lines.size(), rest);
         }
 
         auto find(std::string str)
         {
             size_t iCol = 0, iLine = 0;
 
-            for (size_t i = 0; auto &line : data)
+            for (size_t i = 0; auto &line : lines)
             {
                 auto pos = line.find(str);
                 if (pos != std::string::npos)
